258617f3b58853a9c994f4651ba710d4cc0c5a45
LadyCrystal <crystaledemahadzi@gmail.com>
2023-04-24 17:04:20 +0200

    initial commit

---
+++ src/logic/models/Product.js
@@ -0,0 +1,29 @@
+export default class Product {
+  constructor(
+    sku,
+    name,
+    description,
+    price,
+    category,
+    tags,
+    colorOptions,
+    sizeOptions,
+    reviewScore,
+    reviews,
+    mainImagePath,
+    allImages,
+  ) {
+    this.sku = sku;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.category = category;
+    this.tags = tags;
+    this.colorOptions = colorOptions;
+    this.sizeOptions = sizeOptions;
+    this.reviewScore = reviewScore;
+    this.reviews = reviews;
+    this.mainImagePath = mainImagePath;
+    this.allImages = allImages;
+  }
+}
